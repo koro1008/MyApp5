f26dafc9cd2ba26f2d5f494ea535c83f5b7e6200
koro saka <koro.nn99@gmail.com>
2019-06-02 20:48:56 +0900

    レイアウトパーツ書き出し

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Picker, TextInput, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,30 +20,147 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={styles.parentView}>
+        <View style={styles.headPart}>
+          <View style={styles.firstLine}>
+            <Text style={styles.currentNumText}>---</Text>
+            <Picker style={styles.picker}>
+              <Picker.Item style={styles.pickerItem} label="投" value={1}/>
+              <Picker.Item style={styles.pickerItem} label="捕" value={2}/>
+              <Picker.Item style={styles.pickerItem} label="一" value={3}/>
+              <Picker.Item style={styles.pickerItem} label="二" value={3}/>
+              <Picker.Item style={styles.pickerItem} label="三" value={3}/>
+              <Picker.Item style={styles.pickerItem} label="遊" value={3}/>
+              <Picker.Item style={styles.pickerItem} label="左" value={3}/>
+              <Picker.Item style={styles.pickerItem} label="中" value={3}/>
+              <Picker.Item style={styles.pickerItem} label="右" value={3}/>
+            </Picker>
+            <TextInput style={styles.inputName}></TextInput>
+            <View style={styles.buttonContainer}>
+              <Button title="クリア" color='#0000000'></Button>
+            </View>
+          </View>
+          <View style={styles.firstLine}>
+            <Button title="キャンセル"></Button>
+            <Button title="登録"></Button>
+            <Button title="入れ替え"></Button>
+          </View>
+        </View>
+        <View style={styles.orderPart}>
+          <View style={styles.playerContainer}>
+            <Button style={styles.numberButton} title="１番"></Button>
+            <Text style={styles.position}>---</Text>
+            <Text style={styles.playerName}>----</Text>
+          </View>
+          <View style={styles.playerContainer}>
+            <Button title="２番"></Button>
+            <Text>---</Text>
+            <Text>----</Text>
+          </View>
+          <View style={styles.playerContainer}>
+            <Button title="３番"></Button>
+            <Text>---</Text>
+            <Text>----</Text>
+          </View>
+          <View style={styles.playerContainer}>
+            <Button title="４番"></Button>
+            <Text>---</Text>
+            <Text>----</Text>
+          </View>
+          <View style={styles.playerContainer}>
+            <Button title="５番"></Button>
+            <Text>---</Text>
+            <Text>----</Text>
+          </View>
+          <View style={styles.playerContainer}>
+            <Button title="６番"></Button>
+            <Text>---</Text>
+            <Text>----</Text>
+          </View>
+          <View style={styles.playerContainer}>
+            <Button title="７番"></Button>
+            <Text>---</Text>
+            <Text>----</Text>
+          </View>
+          <View style={styles.playerContainer}>
+            <Button title="８番"></Button>
+            <Text>---</Text>
+            <Text>----</Text>
+          </View>
+          <View style={styles.playerContainer}>
+            <Button title="９番"></Button>
+            <Text>---</Text>
+            <Text>----</Text>
+          </View>
+
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  parentView:{
     flex: 1,
+    backgroundColor: '#3cb371',
+  },
+  headPart:{
+    flex: 1,
+  },
+  firstLine: {
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#ffffe0',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  currentNumText: {
+    fontSize: 10,
+    textAlign: 'left',
+    backgroundColor: '#f5deb3',
   },
-  instructions: {
+  picker: {
+    width: 60,
+    height: 200,
+    backgroundColor: '#FFF',
+  },
+  pickerItem: {
+    fontSize: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  inputName: {
+    width: 60,
+    fontSize: 8,
+    backgroundColor: '#f0e68c',
+  },
+  buttonContainer: {
+    width: 80,
+    height: 50,
+    backgroundColor: '#ffa500',
+  },
+  orderPart:{
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  playerContainer:{
+    flex: 1,
+    flexDirection: 'row',
+    height: 30,
+    alignItems: 'center',
+    backgroundColor: '#b0c4de',
+    margin: 5,
+  },
+  numberButton:{
+    height: 30,
+    margin: 0,
+  },
+  position:{
+    margin: 0,
+    height: 30,
+    backgroundColor: '#FFF',
+  },
+  playerName:{
+    margin: 0,
+    height: 30,
+    backgroundColor: '#FFF',
   },
 });
